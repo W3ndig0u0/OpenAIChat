835ddbf39a0b80da1cc450c87feb26b862cab456
W3ndig0 <70271139+W3ndig0u0@users.noreply.github.com>
2023-02-01 18:17:27 +0100

    saaa

---
+++ OpenAI/app.py
@@ -0,0 +1,31 @@
+import os
+
+import openai
+from flask import Flask, redirect, render_template, request, url_for
+from dotenv import load_dotenv
+
+app = Flask(__name__)
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+
+@app.route("/", methods=("GET", "POST"))
+def index():
+    if request.method == "POST":
+        iceCream = request.form["animal"]
+        response = openai.Completion.create(
+            model="text-davinci-003",
+            prompt=generate_prompt(iceCream),
+            temperature=0,
+        )
+        return redirect(url_for("index", result=response.choices[0].text))
+
+    result = request.args.get("result")
+    return render_template("index.html", result=result)
+
+
+def generate_prompt(iceCream):
+    return """Suggest three names for an Weed to sell in our store depending on the flavour.
+Flavour: {}
+Name:""".format(
+        iceCream.capitalize()
+    )
