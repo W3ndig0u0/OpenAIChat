8f5ae3b144911d850d61ff38a65f7b6844b816f2
W3ndig0 <70271139+W3ndig0u0@users.noreply.github.com>
2023-02-01 18:08:27 +0100

    SHISHISHI

--- app.py
+++ app.py
@@ -1,2 +1,10 @@
-import hello
+import os
+import openai
+
+
 print("HUH?!")
+
+openai.api_key = os.getenv(
+    "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")
+response = openai.Completion.create(
+    model="text-davinci-003", prompt="Say this is a test", temperature=0, max_tokens=7)
